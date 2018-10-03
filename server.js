dc18e90747e32fca891bd365536224b56a6ce51b
Your Name <you@example.com>
2018-10-03 15:33:38 -0700

    all

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ fs.readFile('zipcodeDict.txt', 'utf8', function(err, contents) {
 	zipcodeDict = contents;
 	console.log(Object.keys(contents).length);
 });
-
+app.get('/', function(req,res){
+	res.send(zipcodeDict);
+})
 app.get('/:zipcode', function(req, res) {
 	var zipcode = req.params["zipcode"];
 	if (zipcode in zipcodeDict) {
