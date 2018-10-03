b7e17182fa3157504755c92a888fb2bdab5a6ba4
Your Name <you@example.com>
2018-10-03 15:08:52 -0700

    length

---
+++ server.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+//server.js
+
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+var zipcodeDict = {};
+fs.readFile('zipcodeDict.txt', 'utf8', function(err, contents) {
+	var contents = JSON.parse(contents);
+	zipcodeDict = contents;
+	console.log(Object.keys(contents).length);
+});
+
+app.get('/:zipcode', function(req, res) {
+	var zipcode = req.params["zipcode"];
+	if (zipcode in zipcodeDict) {
+		res.send(zipcodeDict[zipcode]);
+	} else {
+		res.send("Error");
+	}
+});
+
+
+
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
+})
+
